3a69fd39b03910d40448dd62adeeb811c0014091
Andrey Shekunov <ashekunov@gmail.com>
2023-08-17 15:39:17 +0500

    Исправил ошибку названия класса PoligonalModel

--- ArchSem/src/main/java/homework1/ModelElements/PoligonalModel.java
+++ ArchSem/src/main/java/homework1/ModelElements/PoligonalModel.java
@@ -3,11 +3,11 @@ package homework1.ModelElements;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PoliginalModel {
+public class PoligonalModel {
     public List<Texture> textures;
     public List<Poligon> poligonas;
 
-    public PoliginalModel(List<Texture> textures) {
+    public PoligonalModel(List<Texture> textures) {
         this.textures = textures;
         poligonas = new ArrayList<>();
     }
