2cac5c58645dfb9424823f056bf6fb633625eab8
Andrey Shekunov <ashekunov@gmail.com>
2023-08-17 15:46:20 +0500

    Добавил комментарии в класс Camera

--- ArchSem/src/main/java/homework1/ModelElements/Camera.java
+++ ArchSem/src/main/java/homework1/ModelElements/Camera.java
@@ -3,10 +3,15 @@ package homework1.ModelElements;
 import homework1.Stuff.Angle3D;
 import homework1.Stuff.Point3D;
 
+
+//  Класс камер
 public class Camera {
     public Point3D location;
     public Angle3D angle;
 
+    // поворот камеры
     public void Rotate(Angle3D angle){}
-    public void Move(Point3D location){}
+
+    // перемещение камеры
+    public void Move(Point3D point){}
 }
