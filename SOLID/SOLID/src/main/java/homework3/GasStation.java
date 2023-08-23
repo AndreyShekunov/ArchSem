761e1945c527d512e254c5dc6336a25ca8fd6d29
Andrey Shekunov <ashekunov@gmail.com>
2023-08-23 11:56:39 +0500

    Добавить в интерфейс «Заправочная станция» методы: протирка лобового стекла, протирка фар, протирка зеркал.

--- SOLID/SOLID/src/main/java/homework3/GasStation.java
+++ SOLID/SOLID/src/main/java/homework3/GasStation.java
@@ -5,4 +5,16 @@ public interface GasStation {
     // метод заправки топливом
 
     void refueling();
+
+    // метод протирка лобового стекла
+
+    void wipingWindshield();
+
+    // метод протирка фар
+
+    void wipingHeadlights();
+
+    // метод протирка зеркал
+
+    void wipingMirrors();
 }
