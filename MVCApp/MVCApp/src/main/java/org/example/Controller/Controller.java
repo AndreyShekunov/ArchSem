16b0423aca759837e27d6bdb58c2e39f97ac3d50
Andrey Shekunov <ashekunov@gmail.com>
2023-10-08 13:00:07 +0500

    MVP

--- MVCApp/MVCApp/src/main/java/org/example/Controller/Controller.java
+++ MVCApp/MVCApp/src/main/java/org/example/Controller/Controller.java
@@ -1,22 +1,45 @@
 package org.example.Controller;
 
+import org.example.Model.Domen.Student;
 import org.example.Model.Model;
 import org.example.View.View;
+
+import java.util.List;
 
 public class Controller {
     private Model model;
     private View view;
+    private List<Student> students;  //хранилище презентора
 
     public Controller(Model model, View view) {
         this.model = model;
         this.view = view;
     }
 
+    //модель тестирования для презентора
+    private boolean test(List<Student> studs){
+        if(studs.size() > 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     public void update() {
+        //MVP
+        students = model.getAllStudents();
+
+        if (test(students)){
+            view.printAllStudent(students);
+        }else {
+            System.out.println("Модель не доступна!");
+        }
+
+
         //MVC
 
         //Логика контроллера
 
-        view.printAllStudent(model.getAllStudents());
+        //view.printAllStudent(model.getAllStudents());
     }
 }
